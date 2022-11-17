0e46bf7dc6423b0eede1f74dbd2d15791dc359e1
Дмитрий Кравец <gmankrz@gmail.com>
2022-11-17 11:12:42 +0300

    Добавлено подключение к удалённой БД

--- db.js
+++ db.js
@@ -1,12 +1,16 @@
 const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: "postgres",
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-    }
+    "postgres://iukkszge:uh9Bw1OpLRqzk4wI-0nlwZ3jOhoU5fG7@abul.db.elephantsql.com/iukkszge"
 );
+
+// module.exports = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: "postgres",
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//     }
+// );
